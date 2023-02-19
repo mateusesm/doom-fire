2ae5fd2d748ed816619e7157cc8513998017bbe0
Mateus Macedo <mateusemanuel198@gmail.com>
2023-02-19 12:07:14 -0300

    code remade - datas

--- js/script.js
+++ js/script.js
@@ -1,69 +1,71 @@
-function init() {
-
-    const generateDataBase = (arrayFire) => {
-        const width = 10
-        const height = 10
-
+(function init() {
+    const generateFireCanvas = (arrayWithFire) => {
         const divContainer = document.querySelector('.fire-container')
+        const line = 10
+        const column = 10
 
         let index = 0
-
         let table = ''
+
         table = '<table class="fire-data">'
-        for (let i = 0; i < width; i++) {
+
+        for (let i = 0; i < line; i++) {
             table += '<tr>'
-            for (let j = 0; j < height; j++) {
-                table += `<td>${arrayFire[index]}<span>${index}</span></td>`
+            for (let j = 0; j < column; j++) {
+                table += `<td>${arrayWithFire[index]}<span>${index}</span></td>`
                 index++
             }
             table += '</tr>'
         }
-        table += '</table>'
 
+        table += '</table>'
         divContainer.innerHTML = table
  
     }
 
-    const arrayFire = new Array(100)
+    const generateArray = () => {
+        let arrayFire = []
 
-    for (let i = 0; i < arrayFire.length; i++) {
-        if (!arrayFire[i]) arrayFire[i] = 0
+        for (let i = 0; i <= 99; i++) {
+            arrayFire[i] = 0
+        }
+        return arrayFire
     }
 
-    const generateArrayFire = (fireIntensity) => {
+    const addFireIntensity = (fireIntensity, arrayWithoutFire) => {
+        let counter = 0
 
-        let cont = 0
-        for (let j = 99; j >= 0; j--) {
-            if (cont === 10) break
-           
-            if (!arrayFire[j]){
-                arrayFire[j] = fireIntensity
+        for (let i = 99; i >= 0; i--) {
+       
+           if (counter === 10) return arrayWithoutFire
+            
+            if (arrayWithoutFire[i] === 0) {
+                arrayWithoutFire[i] = fireIntensity
+                counter++
             } else {
-
-                if (!arrayFire[j-10]) {
-                    arrayFire[j-10] = arrayFire[j] -1
-                }
-            }
-
-            cont++
+                counter = 0
+                continue
+            }   
+                    
         }
 
-        return arrayFire
-
+        return arrayWithoutFire
     }
 
     const start = () => {
         let fireIntensity = 36
-
+        const arrayWithoutFire = generateArray()
+      
         setInterval(() => {
-            const arrayFire = generateArrayFire(fireIntensity)
-            generateDataBase(arrayFire)
-            console.log('Oi')
-        }, 1000)
+            const arrayWithFire = addFireIntensity(fireIntensity, arrayWithoutFire)
+
+            generateFireCanvas(arrayWithFire)
+            if (fireIntensity === 0) {
+                fireIntensity = 36
+            }
+            fireIntensity--
+        }, 1000) 
     }
 
     start()
-
-}
-
-init()
+})();
