41a0a15b3a33408418206cf4886f09e1e15ec88d
Mateus Macedo <mateusemanuel198@gmail.com>
2023-02-21 14:24:22 -0300

    update in fire linearity

--- js/script.js
+++ js/script.js
@@ -3,19 +3,38 @@
         {"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}
     ]
 
+    const divContainer = document.querySelector('.fire-container')
+    const debug = false
+
+    let line = 0
+    let column = 0
+    let times = 0
+
+    if (debug) {
+        line = 10
+        column = 10
+        times = 1000
+    } else {
+        line = 40
+        column = 40
+        times = 10
+    }
+
     const renderFireCanvas = (fireIntensity, arrayFire) => {
-        const divContainer = document.querySelector('.fire-container')
-        const line = 60
-        const column = 60
-
         let counter = 0
-
-        for (let i = 3599; i >= 0; i--) {
+        for (let i = (line * column) - 1; i >= 0; i--) {
        
-            if (counter === 60) break
+            if (counter === line) break
             
             if (arrayFire[i] === 0) {
-                arrayFire[i] = fireIntensity
+                if (fireIntensity === 36) {
+                    arrayFire[i] = fireIntensity
+                } else {
+                    let decay = Math.floor(Math.random() * 2)
+                    arrayFire[i] = arrayFire[i+10] - decay
+                   
+                    if (arrayFire[i] < 0) arrayFire[i] = 0
+                }
                 counter++
             } else {
                 counter = 0
@@ -30,11 +49,16 @@
         for (let i = 0; i < line; i++) {
             table += '<tr>'
             for (let j = 0; j < column; j++) {
-                const color = fireColorsPalette[arrayFire[index]]
-                const colorString = `${color.r}, ${color.g}, ${color.b}`
+                if (debug) {
+                    table += `<td class="no-pixel">${arrayFire[index]}<span>${index}</span></td>`
+                } else {
+                    const color = fireColorsPalette[arrayFire[index]]
+                    const colorString = `${color.r}, ${color.g}, ${color.b}`
 
-                table += `<td class="pixel" style="background-color: rgb(${colorString});"></td>`
-                index++                            
+                    table += `<td class="pixel" style="background-color: rgb(${colorString});"></td>`   
+                }
+
+                index++                
             }
         }
 
@@ -46,36 +70,32 @@
     const generateArray = () => {
         let arrayFire = []
 
-        for (let i = 0; i <= 3599; i++) {
+        for (let i = 0; i < line * column; i++) {
             arrayFire[i] = 0
         }
         return arrayFire
     }
 
     const start = () => {
-        let decay = 3
         let fireIntensity = 36
         let controlArray = 0
         let arrayFire = generateArray()
         
         setInterval(() => {
-            if (controlArray === 60) {
-                fireIntensity = 36
+            if (controlArray === line) {
                 arrayFire = generateArray()
+                fireIntensity = 36
                 controlArray = 0
             }
 
             renderFireCanvas(fireIntensity, arrayFire)
 
-            fireIntensity = fireIntensity - Math.floor(Math.random() * decay)
-
-            if (fireIntensity < 0) {
-               fireIntensity = 0
-            }
-
             controlArray++
+            fireIntensity--
+
+            if (fireIntensity < 0) fireIntensity = 0
             
-        }, 60)
+        }, times)
             
     }
     start()
